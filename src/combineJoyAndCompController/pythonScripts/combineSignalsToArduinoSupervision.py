59eff4f8b5e9685d8fbbd79043a83e4c1ca474e0
Jacob Guggenheim <jguggenh@mit.edu>
2019-04-22 14:08:06 -0400

    asdf

---
+++ src/combineJoyAndCompController/pythonScripts/combineSignalsToArduinoSupervision.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+import rospy
+import time
+import numpy as np
+from std_msgs.msg import Float32
+from std_msgs.msg import Float32MultiArray
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import WrenchStamped
+
+# define some global variables
+def defineGlobalVars():
+	global humForce
+	humForce = 0
+	global compForce
+	compForce = 0
+	global optoforceReading
+	optoforceReading = 0
+
+
+def humCallback(data):
+	# max force
+	maxHumForce = 75 # newtons
+
+	# make deadband because joy is annoying
+	joyForce = data.axes[1]
+	if abs(data.axes[1]) < .1:
+		joyForce = 0
+
+	global humForce
+	humForce = float(maxHumForce*joyForce)
+
+
+def compCallback(data):
+	# get and assign computer velocity to global
+	global compForce
+	compForce = float(data.data)
+
+
+def optoforceCallback(data):
+	# get and assign computer velocity to global
+	global optoforceReading
+	optoforceReading = -1*float(data.wrench.force.z)
+
+
+def start():
+	# initialize node
+	rospy.init_node('combinedForceToArduino', anonymous = True)
+
+	# init arduino commands pub
+	global cmd2ArduinoPub
+	cmd2ArduinoPub = rospy.Publisher('command2Arduino', Float32, queue_size = 10)
+	global measurement2ArduinoPub
+	measurement2ArduinoPub = rospy.Publisher('measurement2Arduino', Float32, queue_size = 10)
+
+	# command subscribers
+	humSubscriber = rospy.Subscriber("joy", Joy, humCallback)
+	compSubscriber = rospy.Subscriber('compForceTraj', Float32, compCallback)
+
+	# measurement subscribers
+	optoforceSubscriber = rospy.Subscriber('/optoforce_0', WrenchStamped, optoforceCallback)
+
+	# go to send command loop
+	sendCommand()
+
+	# rospy.spin()
+	rospy.spin()
+
+
+def sendCommand():
+	# send send send
+	readyCount = 0
+	pubRate = 50 # hertz
+	constantForceOffset = 0
+	sendTraj = rospy.set_param("sendTraj",0)
+	rate = rospy.Rate(pubRate) # hertz
+
+	while (not rospy.is_shutdown()):
+		# check whether we should be sending trajectory
+		sendTraj = rospy.get_param("sendTraj",0)
+
+		# get the most up to date readings
+		measurement = optoforceReading
+
+		# decide if we are sending the traj
+		if sendTraj == 1:
+			# publish that signal
+			command = (compForce)
+			print('sending')
+			cmd2ArduinoPub.publish(command)
+			measurement2ArduinoPub.publish(measurement)
+
+		else:
+			# send desired force
+			cmd2ArduinoPub.publish(humForce)
+			measurement2ArduinoPub.publish(measurement)
+
+			print()
+
+			if (optoforceReading > constantForceOffset - 1) and (optoforceReading < constantForceOffset + 1):
+				readyCount = readyCount + 1
+				print('counting up to send')
+			else:
+				readyCount = 0
+				print('not in range')
+			if readyCount > 100: # been at the right force for 2 seconds...start
+				sendTraj = rospy.set_param("sendTraj",1)
+				readyCount = 0
+
+		# sleep to maintain loop rate
+		rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+	defineGlobalVars()
+	start()
+    except rospy.ROSInterruptException:
+        pass
---
+++ src/computerForceTrajectory/pythonScripts/computerForceTrajectorySupervision.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from scipy import interpolate
+import math
+import random
+from std_msgs.msg import Float32
+
+def start():
+	# initialize node
+	rospy.init_node('forceTraj', anonymous = True)
+
+	# initialize computer force trajectory publisher
+	global compForceTrajPub
+	compForceTrajPub = rospy.Publisher('compForceTraj', Float32, queue_size = 10)
+
+	# generate trajectory
+	trajPubRate = 100.0; # Hz
+	trajVec = genTraj(trajPubRate)
+
+	# begin sending traj
+	sendTraj(trajPubRate, trajVec)
+
+	# rospy.spin()
+	rospy.spin()
+
+def genTraj(trajPubRate):
+	maxForceD = 15 # newtwons per second
+	minForce = -75
+	maxForce = 75
+
+	trialLength = 60 # seconds
+
+	# random seed
+	random.seed()
+
+	# pick start time (feature 1)...within first 5->15 seconds
+	start_time = int(random.random()*(15-5) + 5)
+	end_time = int(np.array(random.sample(range(-5,5),1))) + 50 # 30 +/- 5 second trial but takes two seconds to get back down to 0
+
+	# pick desired force
+	desired_force = 5
+
+	# now generate reference points for just start and stop of trajectory
+	trajVecSparse = np.zeros(int(trialLength))
+	trajVecSparse[0] = 0
+	for i in range(1,trialLength): # go from 1 because at 0 its set at 0
+		if (i == start_time):
+			trajVecSparse[i] = 0
+		elif (i == start_time + 1):
+			trajVecSparse[i] = 2.5
+		elif (i == start_time + 2):
+			trajVecSparse[i] = 5
+		elif (i == end_time):
+			trajVecSparse[i] = 5
+		elif (i == end_time + 1):
+			trajVecSparse[i] = 2.5
+		elif (i == end_time + 2):
+			trajVecSparse[i] = 0
+		elif (i > start_time and i < end_time):
+			trajVecSparse[i] = 5
+		else:
+			trajVecSparse[i] = 0
+
+	# now add in "failures"...characterized by a sustained (3 seconds) offset
+	max_fails = 3
+	min_fails = 1
+	num_failures = int(random.random()*(max_fails - min_fails) + min_fails)
+
+	# choose failure start time, end times, and direction...then insert into trajVecSparse
+	srl_action_range = range(start_time, end_time)
+	srl_action_range_failures = srl_action_range[0::6]
+	srl_action_range_failures = srl_action_range_failures[1:-1]
+	fail_start_times = random.sample(srl_action_range_failures, num_failures)
+	for i in range(0,num_failures):
+		# get end time and direction
+		fail_end_time = 3 + fail_start_times[i]
+		fail_direction = random.sample([-1, 1], 1)
+
+		# insert into sparse trajectory
+		for j in range(fail_start_times[i],fail_end_time):
+			trajVecSparse[j] = trajVecSparse[j] + 3*np.array(fail_direction)
+
+	print(trajVecSparse)
+
+	# now interpolate at trajPubRate
+	interpFun = interpolate.interp1d(np.arange(0,len(trajVecSparse),1),trajVecSparse,kind="cubic")
+	timeVec = np.arange(0.0,trialLength-1,1/trajPubRate)
+	trajVec = interpFun(timeVec)
+	return trajVec
+
+def sendTraj(trajPubRate, trajVec):
+	# ask lead arm to follow randomized trajectory
+	loopNum = 0
+	rate = rospy.Rate(trajPubRate) # hertz
+	while (not rospy.is_shutdown()):
+		# check whether we should be sending trajectory
+		sendTraj = rospy.get_param("sendTraj",0)
+		if (sendTraj == 1) and (loopNum < len(trajVec)):
+			# do stuff
+			# print(trajVec[loopNum])
+			compForceTrajPub.publish(trajVec[loopNum])
+			loopNum = loopNum + 1
+		else:
+			# send 0
+			compForceTrajPub.publish(0)
+
+		# sleep to maintain loop rate
+		rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        start()
+    except rospy.ROSInterruptException:
+        pass
